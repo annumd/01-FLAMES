b3f58a63521d8a251372247e5b99f89c20d7b76b
annumd <161940207+annumd@users.noreply.github.com>
2024-08-04 19:01:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-function calculateFlames() 
-{
+function calculateFlames() {
     var name1 = document.getElementById("name1").value.toLowerCase().replace(/\s+/g, '');
     var name2 = document.getElementById("name2").value.toLowerCase().replace(/\s+/g, '');
-
+    
     var flames = "FLAMES";
     var relationship = ["Friends", "Love", "Affection", "Marriage", "Enemy", "Siblings"];
 
     // Calculate common letters
-    for (var i = 0; i < name1.length; i++) 
-        {
-          var char = name1[i];
-          if (name2.includes(char)) {
-              name1 = name1.replace(char, '');
-              name2 = name2.replace(char, '');
-              i--; // Restart loop due to updated string
+    for (var i = 0; i < name1.length; i++) {
+        var char = name1[i];
+        if (name2.includes(char)) {
+            name1 = name1.replace(char, '');
+            name2 = name2.replace(char, '');
+            i--; // Restart loop due to updated string
         }
-  }
+    }
 
     // Calculate FLAMES index
     var count = name1.length + name2.length;
     var index = count % flames.length;
 
-    if (index === 0) 
-    {
+    if (index === 0) {
         index = flames.length - 1;
-    } 
-    else 
-    {
+    } else {
         index = index - 1;
     }
     var result = relationship[index];
@@ -38,17 +33,13 @@ var isMuted = false;
 var muteButton = document.getElementById('mute-button');
 var spotifyPlayer = document.getElementById('spotify-player');
 
-muteButton.addEventListener('click', function() 
-  {
+muteButton.addEventListener('click', function() {
     isMuted = !isMuted;
-    if (isMuted) 
-    {
+    if (isMuted) {
         spotifyPlayer.src = "";
         muteButton.querySelector('.mute-icon').innerText = "🔇";
-    } 
-    else 
-    {
-        spotifyPlayer.src = "https://open.spotify.com/embed/track/5RbkI2nfF5VslQAFV2cghY?si=8392c86891ad45c8";
+    } else {
+        spotifyPlayer.src = "https://open.spotify.com/embed/track/43uRGlsrRRGJqaq8ZgmSgA?si=d67ca8a46f064e35";
         muteButton.querySelector('.mute-icon').innerText = "🔈";
     }
 });
